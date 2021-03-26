eb11b54673c2b11621f104253581f2e88504d059
Mariya <mhozyaenko@gmail.com>
2021-03-26 23:52:17 +0200

    add solution 05-http-server-streams 01-file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const server = new http.Server();
 
+const {createReadStream} = require('fs');
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
@@ -11,6 +13,29 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.end('Вложенные папки не поддерживаются');
+      }
+
+      const readStream = createReadStream(filepath);
+
+      readStream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Файл не найден');
+        } else {
+          res.statusCode = 500;
+          res.end('Что-то пошло не так...');
+        }
+      });
+
+      readStream.on('data', (data) => {
+        res.write(data);
+        res.statusCode = 200;
+        res.end();
+      });
 
       break;
 
