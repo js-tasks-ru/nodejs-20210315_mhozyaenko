46f20db0fb6be800b3e37d5e2d1cc75ec68f6ffc
Mariya <mhozyaenko@gmail.com>
2021-03-29 22:32:26 +0300

    add solution 05-http-server-streams 02-file-server-post

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const {createWriteStream, unlink} = require('fs');
+const LimitSizeStream = require('../../03-streams/01-limit-size-stream/LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,6 +13,55 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+
+      if (pathname.includes('/') || pathname.includes('..')) {
+        res.statusCode = 400;
+        res.end('Вложенные папки не поддерживаются');
+      }
+
+      const writeStream = createWriteStream(filepath, {flags: 'wx'});
+      const limitedStream = new LimitSizeStream({
+        limit: 1024 * 1024,
+        highWaterMark: 1024 * 1024 * 2,
+      });
+
+      req.pipe(limitedStream).pipe(writeStream);
+
+      writeStream.on('error', (error) => {
+        writeStream.destroy();
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('Файл уже существует');
+        } else {
+          res.statusCode = 500;
+          res.end('Что-то пошло не так...');
+        }
+      });
+
+      limitedStream.on('error', (error) => {
+        writeStream.destroy();
+        if (error.code === 'LIMIT_EXCEEDED') {
+          unlink(filepath, () => {});
+          res.statusCode = 413;
+          res.end('Размер файла превышает 1МБ');
+        } else {
+          res.statusCode = 500;
+          res.end('Что-то пошло не так...');
+        }
+      });
+
+      writeStream.once('finish', () => {
+        if (writeStream.bytesWritten > 0) {
+          res.statusCode = 201;
+          res.end('OK');
+        }
+      });
+
+      res.on('close', () => {
+        if (res.finished) return;
+        unlink(filepath, () => {});
+        res.destroy();
+      });
 
       break;
 
